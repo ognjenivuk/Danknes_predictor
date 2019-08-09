d7765d8ff05ecc8ea8bbd7b52ee29fd452d30260
Ognjen Milinkovic <ognjen7amg@gmail.com>
2019-08-09 10:45:45 +0200

    train-test split added and code refactro
    Co-authored-by: Nikola Bebic <nikola.bebic99@gmail.com>

--- model.py
+++ model.py
@@ -3,7 +3,14 @@ import tensorflow as tf
 import os
 import json
 import math
+import random
 import itertools
+
+def _test_train_split(data: list, split = 0.8):
+    data_copy = data.copy()
+    random.shuffle(data_copy)
+    split_index = int(len(data_copy) * split)
+    return data_copy[:split_index], data_copy[split_index:]
 
 class Model:
     def __init__(self):
@@ -17,10 +24,9 @@ class Model:
         image_encoded = tf.placeholder(tf.float32, shape = (None, 2048))
         sentence_encoded = tf.placeholder(tf.float32, shape = (None, 512))
         encoded_layer = tf.concat([image_encoded, sentence_encoded], axis = 1)
-        first_layer  = tf.layers.dense(encoded_layer, units = 1024, activation=tf.nn.relu)
-        second_layer  = tf.layers.dense(first_layer , units = 512,  activation=tf.nn.relu)
-        third_layer  = tf.layers.dense(second_layer , units = 256,  activation=tf.nn.relu)
-        output  = tf.layers.dense(third_layer, units = 1 )
+        first_layer  = tf.layers.dense(encoded_layer , units = 512,  activation=tf.nn.relu)
+        second_layer  = tf.layers.dense(first_layer , units = 256,  activation=tf.nn.relu)
+        output  = tf.layers.dense(second_layer, units = 1 )
         return image_encoded, sentence_encoded, output
 
     def calculate_loss(self, output, truth):
@@ -30,46 +36,66 @@ class Model:
         return tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
     
     def train(self, number_of_steps):
-        data = Data()
+        self.data = Data()
         self.sess.run(tf.global_variables_initializer())
         for _ in range(number_of_steps):
-            for batch in data.batches():
+            for batch in self.data.batches(is_training = True):
                 _, l = self.sess.run((self.optimizer, self.loss), \
                     feed_dict={self.image_encoded: batch['image'], self.sentence_encoded: batch['sentence'], self.scores: batch['scores']})
                 print(l)
+    def test(self):
+        for batch in self.data.batches(is_training = False, number_of_sampels=len(self.data.test_data)):
+            test_loss = self.sess.run(self.loss, \
+                feed_dict={self.image_encoded: batch['image'], self.sentence_encoded: batch['sentence'], self.scores: batch['scores']})
+        return test_loss
+
+def _read_vectors_from_files(file_dir):
+    encodings = {}
+    for vector_file in os.listdir(file_dir):
+        with open(os.path.join(file_dir, vector_file), 'r') as f:
+            encodings[os.path.splitext(vector_file)[0]] = [float(x) for x in f.read().split(' ')]
+
+    return encodings
+
+
 class Data:
     def __init__(self):
-        self.image_encoding_dir = './Data/image_feature_vectors'
-        self.image_encodings = {}
-        for file in os.listdir(self.image_encoding_dir):
-            with open(os.path.join(self.image_encoding_dir,file), 'r') as f:
-                vector = [float(x) for x in f.read().split(' ')]
-            self.image_encodings[file[:-4]] = vector
+        image_encoding_dir = './Data/image_feature_vectors'
+        self.image_encodings = _read_vectors_from_files(image_encoding_dir)
         
-        self.sentence_encoding_dir = './Data/sentence_feature_vectors'
-        self.sentence_encodings = {}
-        for file in os.listdir(self.sentence_encoding_dir):
-            with open(os.path.join(self.sentence_encoding_dir,file), 'r') as f:
-                vector = [float(x) for x in f.read().split(' ')]
-            self.sentence_encodings[file[:-4]] = vector
+        sentence_encoding_dir = './Data/sentence_feature_vectors'
+        self.sentence_encodings = _read_vectors_from_files(sentence_encoding_dir)
         
-        self.json_file = './Data/db.json'
-        json_file_loaded = json.load(open(self.json_file))
+        json_file = './Data/db.json'
+        with open(json_file) as f:
+            json_file_loaded = json.load(f)
 
         valid_keys = self.image_encodings.keys() & self.sentence_encodings.keys()
 
         self.scores = {v['id'] : math.log(v['ups']) for v in json_file_loaded['_default'].values() if v['id'] in valid_keys}
-        self.data = [(self.image_encodings[x],self.sentence_encodings[x],self.scores[x]) for x in valid_keys if x in self.image_encodings and x in self.sentence_encodings and x in self.scores]
-    def batches(self, number_of_sampels = 100):
+        self.data = [
+            (self.image_encodings[x],self.sentence_encodings[x],self.scores[x])
+            for x in valid_keys
+            if x in self.image_encodings and x in self.sentence_encodings and x in self.scores
+        ]
+
+        self.train_data, self.test_data = _test_train_split(self.data)
+
+
+    def batches(self, is_training ,number_of_sampels = 100):
         batch = {'image':[],'sentence':[],'scores':[]}
-        for i in self.data:
+        current_data = self.train_data if is_training else self.test_data
+        for i in current_data:
             batch['image'].append(i[0])
             batch['sentence'].append(i[1])
             batch['scores'].append([i[2]])
             if len(batch) == number_of_sampels:
                 yield batch
                 batch = {'image':[],'sentence':[],'scores':[]}
-        yield batch
+        if batch['image']:
+            yield batch
 
 model = Model()
-model.train(100)
+model.train(10)
+test_loss = model.test()
+print(f'test loss {test_loss}')

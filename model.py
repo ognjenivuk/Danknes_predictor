fe8fc67537a9ef842657ad4ba4e0fb98f1028792
Ognjen Milinkovic <ognjen7amg@gmail.com>
2019-08-09 16:21:40 +0200

    smoke test passed
    Co-authored-by: Nikola Bebic <nikola.bebic99@gmail.com>

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ import itertools
 from matplotlib import pyplot as plt
 import numpy as np
 from datetime import datetime
-from data import read_vectors_from_files, load_scores
+from data import read_vectors_from_files, load_scores, plot_confusion_matrix
 
 def _test_train_split(data: list, split = 0.8):
     data_copy = data.copy()
@@ -41,48 +41,62 @@ class Model:
     def get_optimizer(self, loss, learning_rate = 0.01):
         return tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
 
-    def _run_batch(self, batch, do_optimize=False):
+    def _run_batch(self, batch, fetches):
         '''runs a batch and returns the loss'''
-        fetches = (self.optimizer, self.loss) if do_optimize else self.loss
-        result = self.sess.run(
+        return self.sess.run(
             fetches, 
             feed_dict={self.image_encoded: batch['image'], self.sentence_encoded: batch['sentence'], self.scores: batch['scores']})
             
-        if do_optimize:
-            return result[1]
-        else:
-            return result
-    
+    def _plot_train_model(self):
+        for batch in self.data.train_batches(number_of_samples=-1):
+            output = self._run_batch(batch, self.output)
+            truth = np.array(batch['scores'])
+
+        plt.plot(output, output, 'r-')
+        plt.plot(output, truth, '.')
+        plt.show()
+
     def train(self, number_of_steps, save_name):
         self.data = Data()
         self.sess.run(tf.global_variables_initializer())
-        saver = tf.train.Saver()
-        save_path = './trained_models/model_' + save_name + str(datetime.now()) +'.cpkt'
+
+        if save_name:
+            saver = tf.train.Saver()
+            save_path = './trained_models/model_' + save_name + str(datetime.now()) +'.cpkt'
         
         training_losses = []
         validation_losses = []
-        for cur_step in range(number_of_steps):
 
-            this_epoch_losses = []
+        try:
+            for cur_step in range(number_of_steps):
 
-            for batch in self.data.train_batches(number_of_samples=1000):
-                l = self._run_batch(batch, do_optimize=True) 
-                this_epoch_losses.append(l)
+                this_epoch_losses = []
 
-            training_losses.append(sum(this_epoch_losses) / len(this_epoch_losses))
-            
-            for batch in self.data.validation_batches():
-                validation_loss = self._run_batch(batch)
-            validation_losses.append(validation_loss)
-            progress = '=' * math.ceil(cur_step / number_of_steps * 80) + '>'
-            progress = progress.ljust(80)
-            progress = progress[:80]
-            print('[', progress, ']', 
-                f' [\x1b[31mtrain loss : {training_losses[-1]:.4f}\x1b[0m' 
-                f', \x1b[32mval loss : {validation_loss:.4f}\x1b[0m]                                  ', end='\r', sep='')
-        saver.save(self.sess, save_path)
-        print()
-        print('model saved at ' + save_path)
+                for batch in self.data.train_batches(number_of_samples=1000):
+                    _, l = self._run_batch(batch, (self.optimizer, self.loss)) 
+                    this_epoch_losses.append(l)
+
+                training_losses.append(sum(this_epoch_losses) / len(this_epoch_losses))
+                
+                for batch in self.data.validation_batches():
+                    validation_loss = self._run_batch(batch, self.loss)
+                validation_losses.append(validation_loss)
+                progress = '=' * math.ceil(cur_step / number_of_steps * 80) + '>'
+                progress = progress.ljust(80)
+                progress = progress[:80]
+                print('[', progress, ']', 
+                    f' [\x1b[31mtrain loss : {training_losses[-1]:.4f}\x1b[0m' 
+                    f', \x1b[32mval loss : {validation_loss:.4f}\x1b[0m]                                  ', end='\r', sep='')
+                    
+            print()
+        except KeyboardInterrupt:
+            pass
+
+        self._plot_train_model()
+
+        if save_name:
+            saver.save(self.sess, save_path)
+            print('model saved at ' + save_path)
         
         return training_losses, validation_losses
     
@@ -92,7 +106,31 @@ class Model:
                 feed_dict={self.image_encoded: batch['image'], self.sentence_encoded: batch['sentence'], self.scores: batch['scores']})
         return test_loss
 
+    def _are_you_dank(self, score):
+        return (np.ones_like(score, dtype=np.int32) * 3) - (score < 7.3) - (score < 8.9) - (score < 10.6)
 
+    def test_classy(self):
+        for batch in self.data.test_batches():
+            output = self._run_batch(batch, self.output)
+            truth = np.array(batch['scores'])
+
+        plt.plot(output, output, 'r-')
+        plt.plot(output, truth, '.')
+        plt.title('this is loss: ' + str(np.mean(np.square(output - truth))))
+        plt.show()
+
+        true_classes = self._are_you_dank(truth).astype(np.int32).flatten()
+        output_classes = self._are_you_dank(output).astype(np.int32).flatten()
+
+        print(true_classes)
+        print(output_classes)
+        
+        plot_confusion_matrix(
+            true_classes, 
+            output_classes, 
+            np.array([0, 1, 2, 3]))
+
+        plt.show()
 
 class Data:
     def __init__(self):
@@ -110,14 +148,14 @@ class Data:
             if x in self.sentence_encodings and x in self.scores
         ]
 
-        self.train_data, self.test_data = _test_train_split(self.data)
+        self.train_data, self.test_data = _test_train_split(self.data, 0.2)
         self.train_data, self.validation_data = _test_train_split(self.train_data, 0.75)
 
     def train_batches(self, number_of_samples = 100):
         return self._get_batch(self.train_data, number_of_samples)
 
     def test_batches(self, number_of_samples = -1):
-        return self._get_batch(self.train_data, number_of_samples)
+        return self._get_batch(self.test_data, number_of_samples)
 
     def validation_batches(self, number_of_samples = -1):
         return self._get_batch(self.validation_data, number_of_samples)
@@ -145,14 +183,17 @@ def main():
     tf.random.set_random_seed(seed)
 
     model = Model(use_text=True)
-    train_losses, validation_losses = model.train(300, save_name=SAVE_NAME_ALL)
+    train_losses, validation_losses = model.train(2000, save_name=SAVE_NAME_ALL)
     plt.plot(np.clip(train_losses, 0, 70))
     plt.plot(np.clip(validation_losses, 0, 70))
     plt.legend(['train', 'validation'])
     plt.title('is this loss')
     plt.show()
+    
     test_loss = model.test()
     print(f'test loss {test_loss}')
+
+    model.test_classy()
     
 if __name__ == '__main__':
     main()
